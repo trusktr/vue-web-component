3592bd96bd5253178f4fad3ae18dbe8c5b2c6beb
Joe Pea <joe@trusktr.io>
2018-02-17 14:31:27 -0800

    alright, the test file works, the builder-js-package loads the @skatejs/ssr/jest environment to add the DOM APIs, and the test loads a Vue-based Web Component

--- src/test.js
+++ src/test.js
@@ -1,12 +1,31 @@
-import '@skatejs/ssr/register' // creates DOM APIs globally
 import { mount } from '@skatejs/bore'
 import { h } from '@skatejs/val'
 import VueWebComponent from '.'
 
-test('testing works', () => {
+const template = h('div', {}, '{{msg}} {{name}}!')
 
-    // TODO
-    console.log(h('div').outerHTML)
+const TestVue = {
+    template,
+    props: {
+        name: {
+            type: String,
+            default: 'World',
+        },
+    },
+    data: () => (console.log('SET UP THE DATA'), {
+        msg: 'Hello'
+    }),
+}
 
-    expect(VueWebComponent).toBe(VueWebComponent)
+const TestElement = VueWebComponent( TestVue )
+customElements.define('test-element', TestElement)
+
+test('VueWebComponent props', done => {
+    const el = document.createElement('test-element')
+
+    setTimeout(() => {
+        console.log('TestElement content:', el.outerHTML)
+        expect( el.nodeName.toLowerCase() ).toBe( 'test-element' )
+        done()
+    }, 1000)
 })
